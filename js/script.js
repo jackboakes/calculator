0beca99a1c91e59b1fd7b0d7a0a8962c12435e7b
jackboakes <jackboakes98@gmail.com>
2026-09-23 17:08:21 +1000

    Added all the buttons for a basic calculator and styled to a fixed size

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-let operator = "";
-let operandA = "";
-let operandB = "";
+let operator;
+let operandA;
+let operandB;
+
+const displayText = document.querySelector(".display-text");
 
 function add(a, b) {
     return a + b;
@@ -18,6 +20,18 @@ function divide(a, b) {
     return a / b;
 }
 
-function operator(operator, a, b) {
-
+function operate(operator, a, b) {
+    return operator(a, b);
 }
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", () => {
+    displayText.textContent = "";
+    operandA = undefined;
+    operandB = undefined;
+})
+
+console.log(add(2, 7));
+console.log(subtract(5,5,));
+console.log(multiply(2,10));
+console.log(divide(50, 10));
